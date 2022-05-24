d68e1f1f2a8318957a81cc626cfb5c8aa31e7489
CRH-Angie <a.ramirez@creativehope.co.jp>
2022-05-24 23:03:05 +0800

    easy js practice

--- HTML_CSS/scripts/main.js
+++ HTML_CSS/scripts/main.js
@@ -1,2 +1,22 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+let cake = 'chocolate';
+if (cake = 'chocolate') {
+    console.log('I love Chocolate cake');
+} else console.log('What flavor is the cake?');
+
+function multiply(num1, num2) {
+    let product = num1 * num2;
+    return product;
+}
+function addition(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+function newText(txt) {
+    return txt;
+}
+document.querySelector('html').addEventListener('click', function() {
+    alert('stop clicking!');
+})
