756f910a5438bdfee3280892494cccd48095a451
CRH-Angie <a.ramirez@creativehope.co.jp>
2022-05-26 10:56:15 +0800

    tests

--- JavaScript/js/main.js
+++ JavaScript/js/main.js
@@ -1,8 +1,13 @@
 function changeContent() {
-  document.getElementById('changeContent').innerHTML = 'This is a demo for changing Content';
+  // document.getElementById('changeContent').innerHTML = 'This is a demo for changing Content';
+  document.getElementById('changeContent').textContent = 'This is a demo for changing text using textContent'
 }
 function changeImageSrc() {
-  document.getElementById('changeImageSrc').src='images/naruto.jpeg';
+  // document.getElementById('changeImageSrc').src='images/naruto.jpeg';
+  let imgSrc = document.getElementById('changeImageSrc');
+  if (imgSrc.getAttribute('src') == 'images/naruto1.jpeg') {
+    imgSrc.setAttribute('src', 'images/naruto.jpeg');
+  } else imgSrc.setAttribute('src', 'images/naruto1.jpeg');
 }
 function changeTextColor() {
   document.getElementById('changeTextColor').style.color='pink';
@@ -13,14 +18,15 @@ function hideText() {
 function showText() {
   document.getElementById('showText').style.display = 'block'
 }
-// function hideShowText() {
-//   if(document.getElementById('hideShowText').innerHTML == 'Hide this text') {
-//     document.getElementById('hideShowText').style.display = 'none';
-//     document.getElementById('hideShowText').innerHTML = 'This text is finally Shown';
-//     // document.getElementById('hideShowText')
-//   } else {
-//     document.getElementById('hideShowText').style.display = 'block';
-//     document.getElementById('hideShowText').innerHTML = 'This text is finally Shown';
-//     document.getElementById('hideShowText').innerHTML = 'Hide this text';
-//   }
-// }
+function hideShowText() {
+  if(document.getElementById('hideShowText').innerHTML == 'Hide this text') {
+    document.getElementById('hideShowText').style.display = 'none';
+    document.getElementById('hideShowText').innerHTML = 'This text is finally Shown';
+    document.querySelector('#hideShow').textContent = 'Show Text';
+  } else {
+    document.querySelector('#hideShow').textContent = 'Hide Text';
+    document.getElementById('hideShowText').style.display = 'block';
+    document.getElementById('hideShowText').innerHTML = 'This text is finally Shown';
+    document.getElementById('hideShowText').innerHTML = 'Hide this text';
+  }
+}
