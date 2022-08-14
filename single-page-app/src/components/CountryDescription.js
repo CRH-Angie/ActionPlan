bfabc3778a34c60ad56e5545b1481630f55fbf0d
CRH-Angie <a.ramirez@creativehope.co.jp>
2022-08-14 14:14:41 +0800

    prettier

--- single-page-app/src/components/CountryDescription.js
+++ single-page-app/src/components/CountryDescription.js
@@ -1,49 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function CountryDescription() {
-    const [countryDescription, setCountryDescription] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const {cntry} = useParams()
+  const [countryDescription, setCountryDescription] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { cntry } = useParams();
 
-    function fetchCountryDescription() {
-        fetch(`https://restcountries.com/v2/name/kuwait`)
-        .then((res) => res.json())
-        .then((data) => {
-            setCountryDescription(data)
-            setIsLoading(false)
-        })
-        console.log('cntry',cntry)
-
-    }
-    useEffect(()=>{
-        fetchCountryDescription()
-    }, [cntry])
-    if(isLoading){
-        return (
-            <>
-            <div className='country--description'>
-                <h2>Country</h2>
-            </div>
-            </>
-        )
-    } else { 
-        return (
-            <>
-            <div className='country--description'>
-                <h2>Country</h2>
-                <h3>{cntry}</h3>
-                {countryDescription.map((item,index) => {
-                    return (
-                        // <figure>
-                        //     {/* <img src={item.flags.svg} alt={item.name} /> */}
-                        //     <figcaption>{item.name}</figcaption>
-                        // </figure>
-                        <h3 key={index}>{item.name}</h3>
-                    )
-                })}
-            </div>
-            </>
-        )
-    }
+  function fetchCountryDescription() {
+    fetch(`https://restcountries.com/v2/name/kuwait`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryDescription(data);
+        setIsLoading(false);
+      });
+    console.log("cntry", cntry);
+  }
+  useEffect(() => {
+    fetchCountryDescription();
+  }, [cntry]);
+  if (isLoading) {
+    return (
+      <>
+        <div className="country--description">
+          <h2>Country</h2>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="country--description">
+          <h2>Country</h2>
+          <h3>{cntry}</h3>
+          {countryDescription.map((item, index) => {
+            return (
+              // <figure>
+              //     {/* <img src={item.flags.svg} alt={item.name} /> */}
+              //     <figcaption>{item.name}</figcaption>
+              // </figure>
+              <h3 key={index}>{item.name}</h3>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 }
--- single-page-app/src/components/CountryList.js
+++ single-page-app/src/components/CountryList.js
@@ -1,53 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import {Link, useParams} from 'react-router-dom';
-import Pagination from './Pagination';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Pagination from "./Pagination";
 
 export default function CountryList() {
-    const [countryList, setCountryList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true)
-    const [activeLink, setActiveLink] = useState(0)
+  const [countryList, setCountryList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [activeLink, setActiveLink] = useState(0);
 
-    const [currentPage, setCurrentPage] = useState(2);
-    const [postsPerPage] = useState(20);
+  const [currentPage, setCurrentPage] = useState(2);
+  const [postsPerPage] = useState(20);
 
-    function fetchCountryList() {
-        fetch('https://restcountries.com/v3.1/all')
-        .then((res) => res.json())
-        .then((data) => {
-            setCountryList(data)
-            setIsLoading(false)
-        })
-    }
-    
-    useEffect(() => {
-        fetchCountryList()
-    },[])
+  function fetchCountryList() {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryList(data);
+        setIsLoading(false);
+      });
+  }
 
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = countryList.slice(indexOfFirstPost, indexOfLastPost);
-    const howManyPages = Math.ceil(countryList.length/postsPerPage);
+  useEffect(() => {
+    fetchCountryList();
+  }, []);
 
-    if (isLoading) {
-        return (
-            <div className='country--list'>
-                <h2>Countries List</h2>
-            </div>
-        )
-    } else {
-        return (
-            <div className='country--list'>
-                <h2>Countries List</h2>
-    
-                <ul>
-                {currentPosts.map((item, index) => (
-                    <li key={index} className={`${activeLink === index ? 'active' : ''}`}>
-                        <Link  className={`${activeLink === index ? 'active' : ''}`} to={`/country/${item.name.common}`} onClick={() => setActiveLink(index)}>{item.name.common}</Link>
-                    </li>
-                ))}
-                </ul>
-                <Pagination pages={howManyPages} setCurrentPage={setCurrentPage}  />
-            </div>
-        )
-    }
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = countryList.slice(indexOfFirstPost, indexOfLastPost);
+  const howManyPages = Math.ceil(countryList.length / postsPerPage);
+
+  if (isLoading) {
+    return (
+      <div className="country--list">
+        <h2>Countries List</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="country--list">
+        <h2>Countries List</h2>
+
+        <ul>
+          {currentPosts.map((item, index) => (
+            <li
+              key={index}
+              className={`${activeLink === index ? "active" : ""}`}
+            >
+              <Link
+                className={`${activeLink === index ? "active" : ""}`}
+                to={`/country/${item.name.common}`}
+                onClick={() => setActiveLink(index)}
+              >
+                {item.name.common}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Pagination pages={howManyPages} setCurrentPage={setCurrentPage} />
+      </div>
+    );
+  }
 }
