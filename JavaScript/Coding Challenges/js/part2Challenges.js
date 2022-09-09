18df1f8148daec568c0757659706de25ee11dc0b
CRH-Angie <a.ramirez@creativehope.co.jp>
2022-09-09 14:16:28 +0800

    correction for checkWinner function

--- JavaScript/Coding Challenges/js/part2Challenges.js
+++ JavaScript/Coding Challenges/js/part2Challenges.js
@@ -15,14 +15,19 @@ Test Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 */
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-function checkWinner (avgDolphins, avgKoalas) {
-    avgDolphins = calcAverage(85, 54, 41);
-    avgKoalas = calcAverage(23, 34, 27);
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
 
+function checkWinner (avgDolphins, avgKoalas) {
     if (avgDolphins >= avgKoalas * 2) {
-        return console.log(`Dolphin wins (${avgDolphins} vs. ${avgKoalas})`);
+        return console.log(`Dolphin wins! (${avgDolphins} vs. ${avgKoalas})`);
     } else if ( avgKoalas > avgDolphins ** 2){
-        return console.log(`Koalas wins (${avgKoalas} vs. ${avgDolphins})`);
+        return console.log(`Koalas wins! (${avgKoalas} vs. ${avgDolphins})`);
     } else return console.log('NO Team Wins 🥺');
 }
-checkWinner();
+checkWinner(avgDolphins, avgKoalas);
+
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+
+checkWinner(avgDolphins, avgKoalas);
