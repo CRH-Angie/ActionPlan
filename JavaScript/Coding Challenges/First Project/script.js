055afe20094f6bf4bbdb3abb8f7d1089ff699051
CRH-Angie <a.ramirez@creativehope.co.jp>
2022-10-04 19:54:54 +0800

    Guess my number game

--- JavaScript/Coding Challenges/First Project/script.js
+++ JavaScript/Coding Challenges/First Project/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
@@ -8,3 +8,37 @@ document.querySelector('.score').textContent = 10;
 
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Loose!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Loose!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
