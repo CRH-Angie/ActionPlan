6c1da4e8d131e602f7eb5a3130ecf1d052bc8d62
CRH-Angie <a.ramirez@creativehope.co.jp>
2022-10-05 15:02:35 +0800

    GUESS MY NAME Game additions and corrections

--- JavaScript/Coding Challenges/First Project/script.js
+++ JavaScript/Coding Challenges/First Project/script.js
@@ -12,6 +12,7 @@ document.querySelector('.guess').value = 23;
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,6 +23,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -57,9 +62,9 @@ document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('body').style.width = '15rem';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
   document.querySelector('.message').textContent = 'Start Guessing...';
 });
