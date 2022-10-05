129d5c128d0ca9f1dd4e4a8163d165acc9533afd
CRH-Angie <a.ramirez@creativehope.co.jp>
2022-10-05 16:17:52 +0800

    Refactor Guess my name game

--- JavaScript/Coding Challenges/First Project/script.js
+++ JavaScript/Coding Challenges/First Project/script.js
@@ -1,70 +1,59 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+const displayNumber = number => {
+  document.querySelector('.number').textContent = number;
+};
+const displayScore = score => {
+  document.querySelector('.score').textContent = score;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No Number!';
+    displayMessage('⛔️ No Number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
-    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 Correct Number!');
+    displayNumber(secretNumber);
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '💥 You Loose!';
-      document.querySelector('.score').textContent = 0;
+      displayMessage('💥 You Loose!');
+      displayScore(0);
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
+      displayMessage('📉 Too low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '💥 You Loose!';
-      document.querySelector('.score').textContent = 0;
+      displayMessage('💥 You Loose!');
+      displayScore(0);
     }
   }
 });
-
-// Coding Challenge #1
-/*
-Implement a game rest functionality, so that the player can make a new guest! Here is how:
-
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and number variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color(#222) and number width(15rem)
-*/
 
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
+  display(score);
+  displayNumber('?');
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start Guessing...';
+  displayMessage('Start Guessing...');
 });
